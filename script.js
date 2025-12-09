f974607df9eba54000879f6c9765ef518eb2a7e7
Umidahon-umka <sultonboevaumidaxon@gmail.com>
2025-12-09 11:43:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let name = document.getElementById("name");
+let price = document.getElementById("price");
+let color = document.getElementById("color");
+let year = document.getElementById("year");
+let image = document.getElementById("image");
+let addBtn = document.getElementById("addBtn");
+let productList = document.getElementById("productList");
+
+let mashinalar = [];
+
+console.log(mashinalar);
+
+function addData() {
+  let newCar = {
+    name: name.value,
+    price: price.value,
+    color: color.value,
+    year: year.value,
+    image: image.value,
+  };
+
+  mashinalar.push(newCar);
+}
+
+addBtn.addEventListener("click", () => {
+  addData();
+  renderData();
+  console.log(mashinalar);
+});
+
+function renderData() {
+  productList.innerHTML = "";
+
+  mashinalar.forEach((car) => {
+    let div = document.createElement("div");
+    div.className = "card"
+    div.innerHTML = `
+            <img width="200px" class="text" src=${car.image}/>
+            <h2>${car.name}</h2>
+            <p> price:${car.price}</p>
+            <P> color:${car.color}</P>
+            <p> year:${car.year}</p>
+        `;
+
+    productList.appendChild(div);
+  });
+
+  name.value = ""
+  price.value = ""
+  color.value = ""
+  year.value = ""
+  image.value = ""
+}
+
+renderData();
